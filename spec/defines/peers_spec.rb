a6304648f38e22f1e15067c0e1c991c312258dd0
Chris Portman <chris.portman@optusnet.com.au>
2015-04-08 10:28:21 +1000

    tests

--- spec/defines/peers_spec.rb
+++ spec/defines/peers_spec.rb
@@ -6,10 +6,16 @@ describe 'haproxy::peers' do
   context "when no options are passed" do
     let(:title) { 'bar' }
 
-    it { should contain_datacat__fragment('bar_peers_block').with(
-      'order'   => '30-peers-00-bar',
+    it { should contain_datacat_fragment('bar_peers_block').with(
       'target'  => '/etc/haproxy/haproxy.cfg',
-      'content' => "\npeers bar\n"
+      'data'    => {
+        'peers' => {
+          'bar' => {
+            'config'  => "\npeers bar\n",
+            'members' => [],
+          },
+        },
+      }
     ) }
   end
 end
